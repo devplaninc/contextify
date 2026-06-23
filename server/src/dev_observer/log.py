4eb20be999f45a4ddc23c07cee3ee13f94da1ef2
matt <matt@devplan.com>
2026-06-23 16:40:40 -0700

    Updating logging for contextify so debug logs don't show as errors

--- server/src/dev_observer/log.py
+++ server/src/dev_observer/log.py
@@ -1,5 +1,8 @@
 import dataclasses
 import json
+import logging
+import os
+import sys
 from abc import abstractmethod
 from typing import Protocol
 
@@ -33,13 +36,53 @@ class JSONEncoder(Encoder):
 
 
 class PlainTextEncoder(Encoder):
+    _color: bool
+
+    def __init__(self, color: bool = False):
+        self._color = color
 
     def encode(self, msg: "StructuredMessage"):
-        extra = " ".join(f"\033[32m[{k}]={v}\033[0m" for k, v in msg.kwargs.items())
+        if self._color:
+            extra = " ".join(f"\033[32m[{k}]={v}\033[0m" for k, v in msg.kwargs.items())
+        else:
+            extra = " ".join(f"[{k}]={v}" for k, v in msg.kwargs.items())
         return f"{msg.message} {extra}"
 
 
 encoder: Encoder = JSONEncoder()
+
+
+class _BelowErrorFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.levelno < logging.ERROR
+
+
+def _log_level_from_env(default: str = "INFO") -> int:
+    level_name = os.getenv("LOG_LEVEL", default).upper()
+    level = logging.getLevelName(level_name)
+    if isinstance(level, int):
+        return level
+    return logging.INFO
+
+
+def configure_logging(default_level: str = "INFO") -> None:
+    formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.addFilter(_BelowErrorFilter())
+    stdout_handler.setFormatter(formatter)
+
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.ERROR)
+    stderr_handler.setFormatter(formatter)
+
+    root_logger = logging.getLogger()
+    root_logger.handlers.clear()
+    root_logger.setLevel(_log_level_from_env(default_level))
+    root_logger.addHandler(stdout_handler)
+    root_logger.addHandler(stderr_handler)
+
 
 class StructuredMessage:
     message: str
@@ -51,5 +94,6 @@ class StructuredMessage:
 
     def __str__(self):
         return encoder.encode(self)
+
 
 s_ = StructuredMessage
